4666844f8d92eccbaa174e95fe59748e86ea93b5
chaitanya4157 <chaitanya4157@gmail.com>
2025-08-20 10:51:59 +0530

    add my first python program

---
+++ python prgram.py
@@ -0,0 +1,28 @@
+'''
+import pandas as pd
+my_list=[2,3,4]
+my_tuple=(5,6,7)
+my_dict={'A':10,'B':20,'C':30}
+df_list=pd.Dataframes(my_list,
+columns=['List values'])
+df_tuple=pd.Dataframes(my_tuple,
+columns=['tuple values']
+df_dic=pd.Dataframes(my_dict,
+columns=['dict values']
+print(my_list)
+print("Dataframe from tuple:")
+print(my_tuple)
+print("Dataframe from dict:")
+print(df_dict)
+'''
+
+import pandas as pd
+l1=pd.series(list1)
+print(l1)
+l2=pd.Dataframe(list1)
+print(l2)
+
+dict2=("keys":[1,2,3,4],"name":["a","b","c","d"}
+
+
+
